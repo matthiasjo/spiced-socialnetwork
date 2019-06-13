df875da5931bef841ac84662b01679d6bd70401e
Matthias <18295017+matthiasjo@users.noreply.github.com>
2019-06-13 19:20:00 +0200

    error handling for profile pictures in user search

--- src/components/profilepic.js
+++ src/components/profilepic.js
@@ -1,22 +1,15 @@
 import React from "react";
-import styled, { css } from "styled-components";
 import { StyledProfilePic } from "../theme/profilepicStyle";
 
 export default function profilePic(props) {
     const user = props.username || "default username";
     const CompName = props.compName || StyledProfilePic;
     return (
-        <CompName src={props.avatar} alt={user} onClick={props.clickHandler} />
+        <CompName
+            onError={e => (e.target.src = "/img/default.png")}
+            src={props.avatar}
+            alt={user}
+            onClick={props.clickHandler}
+        />
     );
 }
-
-// export default function profilePic(props) {
-//     const user = props.username || "default username";
-//     return (
-//         <StyledProfilePic
-//             src={props.avatar}
-//             alt={user}
-//             onClick={props.clickHandler}
-//         />
-//     );
-// }
