04cc58bc45dd1e12b591a176967068a27b177541
Matthias <18295017+matthiasjo@users.noreply.github.com>
2019-06-21 14:22:14 +0200

    sql change

--- sql/chat.sql
+++ sql/chat.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS chat;
 CREATE TABLE chat(
     id SERIAL PRIMARY KEY,
     sender_id INTEGER REFERENCES users(id) NOT NULL,
-    message TEXT (300) NOT NULL CHECK (message <> ''),
+    message TEXT NOT NULL CHECK (message <> ''),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
