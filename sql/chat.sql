119f82e4ed6b2f94871a56143d3de0295ceb8266
Matthias <18295017+matthiasjo@users.noreply.github.com>
2019-08-08 09:40:36 +0200

    updated sql files for dummy data

--- sql/chat.sql
+++ sql/chat.sql
@@ -8,24 +8,24 @@ CREATE TABLE chat(
 );
 
 
-################### FAILED ATTEMPT #####################
-CREATE OR REPLACE FUNCTION check_chat_update()
-RETURNS TRIGGER
-AS $$
-BEGIN
-SELECT chat.id AS msg_id, message, users.id AS user_id,
-    first, last, avatar, chat.created_at
-    FROM chat
-    JOIN users
-    ON sender_id = users.id
-    WHERE chat.id = NEW.id;
-RETURN NEW;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER check_chat_trigger
-AFTER INSERT ON chat
-FOR EACH STATEMENT EXECUTE PROCEDURE check_chat_update();
-
-DROP TRIGGER IF EXISTS check_chat_trigger ON chat;
+-- ################### FAILED ATTEMPT #####################
+-- CREATE OR REPLACE FUNCTION check_chat_update()
+-- RETURNS TRIGGER
+-- AS $$
+-- BEGIN
+-- SELECT chat.id AS msg_id, message, users.id AS user_id,
+--     first, last, avatar, chat.created_at
+--     FROM chat
+--     JOIN users
+--     ON sender_id = users.id
+--     WHERE chat.id = NEW.id;
+-- RETURN NEW;
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
+--
+-- CREATE TRIGGER check_chat_trigger
+-- AFTER INSERT ON chat
+-- FOR EACH STATEMENT EXECUTE PROCEDURE check_chat_update();
+--
+-- DROP TRIGGER IF EXISTS check_chat_trigger ON chat;
